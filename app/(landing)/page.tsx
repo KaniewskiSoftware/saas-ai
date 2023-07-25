addfa3772293b05c2fc4dbed3fea2975128c8440
Patryk Kaniewski <kaniewski.patryk@gmail.com>
2023-07-25 21:32:47 +0200

    auth added with Clerk

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const LandingPage = () => {
-  return <div>LandingPage (Unprotected)</div>;
+  return (
+    <div>
+      LandingPage (Unprotected)
+      <div>
+        <Link href={"/sign-in"}>
+          <Button>Login</Button>
+        </Link>
+      </div>
+      <div>
+        <Link href={"/sign-up"}>
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
